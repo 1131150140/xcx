adae1da66a24bd48882e9fb52c3a620d561fe646
wangyu <1131150140@qq.com>
2018-05-18 22:20:20 +0800

    1.18

---
+++ src/components/button/button.vue
@@ -0,0 +1,32 @@
+<template>
+    <button class="weui-btn" :type="type" :disabled="disabled"></button>
+</template>
+
+<script>
+export default {
+    name: 'mpvue-button',
+    props: {
+        type: {
+            type: String,
+            default: 'primary'
+        },
+        btnMsg: {
+            type: String,
+            default: '页面主操作normal'
+        },
+        disabled: {
+            type: Boolean,
+            default: false
+        },
+        plain: {
+            type: Boolean,
+            default: false
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/button/index.vue
@@ -0,0 +1,16 @@
+<template>
+        <div>
+            <button>123</button>
+        </div>
+</template>
+
+<script>
+import button from '@/components/button/button'
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
